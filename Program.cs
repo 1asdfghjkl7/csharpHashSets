95ecc94091dc004f6273f098ed8417e40cf61333
Jordan Williams <jordanwilliams288@gmail.com>
2018-08-23 09:34:07 -0500

    completed hashset exercise

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> Showroom = new HashSet<string>();
+            Showroom.Add("Camry");
+            Showroom.Add("Accord");
+            Showroom.Add("Mazda 3");
+            Showroom.Add("The Jeep");
+
+            // foreach (string item in Showroom)
+            // {
+            //     Console.WriteLine(item);
+
+            // }
+            Showroom.Add("Mazda 3");
+            // foreach (string item in Showroom)
+            // {
+            //     Console.WriteLine(item);
+
+            // }
+
+            HashSet<string> UsedLot = new HashSet<string>();
+            UsedLot.Add("Fiesta");
+            UsedLot.Add("F-150");
+
+            Showroom.UnionWith(UsedLot);
+            Showroom.Remove("Camry");
+
+            // foreach (string item in Showroom)
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+            HashSet<string> Junkyard = new HashSet<string>();
+            Junkyard.Add("Civic");
+            Junkyard.Add("Ranger");
+            Junkyard.Add("Mazda 3");
+            Junkyard.Add("Accord");
+
+            HashSet<string> clone = new HashSet<string>(Showroom);
+
+            clone.IntersectWith(Junkyard);
+            Showroom.UnionWith(Junkyard);
+            Showroom.Remove("Ranger");
+
+            foreach (string item in Showroom)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
